49c82edeaacaf5faa8d1818b2698cd4f3e50c90e
jeansoto7 <jeancarlossoto7@gmail.com>
2018-08-19 23:55:45 -0400

    Updates

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,42 @@
-var gulp          = require('gulp');
-var childProcess  = require('child_process');
-var shell         = require('gulp-shell');
-var browserSync   = require('browser-sync');
-// var cp            = require('child_process');
-const plumber     = require('gulp-plumber');
-const newer       = require('gulp-newer');
-const jfm         = require('jstransformer-jade-jekyll');
-const minify      = require('gulp-minify');
-const pug         = require('pug');
-var gulpPug       = require('gulp-pug');
+var gulp               = require('gulp');
+var plumber            = require('gulp-plumber');
+var newer              = require('gulp-newer');
+var childProcess       = require('child_process');
+var shell              = require('gulp-shell');
+var browserSync        = require('browser-sync');
+const minify           = require('gulp-minify');
+const pug              = require('pug');
+const jfm              = require('jstransformer-jade-jekyll');
+var gulpPug            = require('gulp-pug');
 
-var prefix        = require('gulp-autoprefixer');
-var sass          = require('gulp-sass');
-var csso          = require('gulp-csso');
-var htmlmin       = require('gulp-htmlmin');
-var jsValidate    = require('gulp-jsvalidate');
-var uglify        = require('gulp-uglify');
-var pump          = require('pump');
+var prefix             = require('gulp-autoprefixer');
+var sass               = require('gulp-sass');
+var CleanCSS           = require('clean-css');
+var csso               = require('gulp-csso');
+var htmlmin            = require('gulp-htmlmin');
+var jshint             = require('gulp-jshint');
+var jsValidate         = require('gulp-jsvalidate');
+var uglify             = require('gulp-uglify');
+var pump               = require('pump');
 
-var scsslint      = require('gulp-scss-lint');
-var bootlint      = require('gulp-bootlint');
-var html5Lint     = require('gulp-html5-lint');
-var htmlhint      = require("gulp-htmlhint");
+var $                  = require('gulp-load-plugins')();
+var sourcemaps         = require('gulp-sourcemaps');
+var reload             = browserSync.reload;
+var scsslint           = require('gulp-scss-lint');
+var bootlint           = require('gulp-bootlint');
+var html5Lint          = require('gulp-html5-lint');
+var htmlhint           = require("gulp-htmlhint");
 
-var imagemin      = require('gulp-imagemin');
-var imageminSvgo  = require('imagemin-svgo');
-var imageop       = require('gulp-image-optimization');
-var rename        = require('gulp-rename');
+var rename             = require('gulp-rename');
+const size             = require('gulp-size');
+
+// Gulp-Imagemin Read Documentation and usage at https://www.npmjs.com/package/gulp-imagemin
+const imagemin         = require('gulp-imagemin');
+const imageminGifsicle = require('imagemin-gifsicle');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminOptipng  = require('imagemin-optipng');
+const imageminSvgo     = require('imagemin-svgo');
+
 
 
 var jekyll      = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
@@ -105,7 +115,7 @@ gulp.task('browser-sync', ['sass', 'jekyll-build'], function() {
 /**
  * Gulp task to minify HTML files
  */
-gulp.task('htmlmin', function() {
+gulp.task('compress-html', function() {
     return gulp.src('_site/**/*.html')
         .pipe(htmlmin({
             collapseWhitespace: true,
@@ -114,10 +124,17 @@ gulp.task('htmlmin', function() {
             collapseBooleanAttributes: true,
             removeRedundantAttributes: true,
             removeEmptyAttributes: true,
-            removeEmptyElements: true,
-            lint: false,
+            // removeEmptyElements: true,
+            removeOptionalTags: true,
+            lint: true,
+            minifyCSS: true,
+            minifyJS: true,
+            minifyURLs: false,
         }))
-        .pipe(gulp.dest('_site/'));
+        .pipe(gulp.dest('_site/'))
+        .pipe(size({
+          title: 'html'
+        }))
 });
 
 
@@ -126,6 +143,7 @@ gulp.task('htmlmin', function() {
  */
 gulp.task('sass', function () {
     return gulp.src('assets/css/main.scss')
+        .pipe(plumber())
         .pipe(sass({
             outputStyle: 'nested',
             precision: 10,
@@ -142,38 +160,38 @@ gulp.task('sass', function () {
         .pipe(gulp.dest('assets/css'));
 });
 
-// gulp.task('build-js', function() {
-//     return gulp.src('_site/js/*.js')
-//         .pipe(jsValidate())
-//         .pipe(uglify())
-//         .pipe(gulp.dest('_site/js/'));
-// });
 
-
-
-
-// gulp.task('jekyll', shell.task(['jekyll build']));
-
-
-gulp.task('build', ['jekyll'], function() {
-    gulp.run('htmlmin');
+/**
+ * Gulp task to minify JS files
+ */
+gulp.task('compress-js', function() {
+  return gulp.src('files/js/**')
+    .pipe(plumber())
+    .pipe(jsValidate())
+    .pipe(uglify())
+    .pipe(gulp.dest('assets/js'))
+    .pipe(size({
+      title: 'js'
+    }))
 });
 
-// check tasks
-gulp.task('check-html', function() {
-    return gulp.src('_site/*.html')
-        .pipe(html5Lint())
-        .pipe(htmlhint())
-        .pipe(bootlint());
-});
-//
-// var scsslint = require('gulp-scss-lint');
-// gulp.task('check-scss', function() {
-//     gulp.src([ 'assets/css/**/*.scss' ])
-//         .pipe(scsslint())
-// });
 
-gulp.task('check', [ 'check-html' ], function() {});
+
+// Optimize Images
+gulp.task('compress-img', function() {
+  return gulp.src('files/img/**')
+    .pipe(plumber())
+    .pipe(newer('assets/img/**'))
+    .pipe(imagemin([
+      imagemin.gifsicle({interlaced: true}),
+      imagemin.jpegtran({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3})
+    ]))
+    .pipe(gulp.dest('assets/img'))
+    .pipe(size({
+      title: 'img'
+    }))
+});
 
 
 
@@ -199,15 +217,123 @@ gulp.task('pug', () => {
      gulp.watch('assets/css/**', ['sass']);
      gulp.watch('assets/js/**', ['jekyll-rebuild']);
      gulp.watch(['*.html', '_layouts/*.html', '_includes/*','assets/**/*', '_posts/*', '_config.yml'], ['jekyll-rebuild']);
+     gulp.watch(['files/js/**'], ['compress-js']);
+     gulp.watch(['files/img/**'], ['compress-img']);
      gulp.watch(['assets/js/**'], ['jekyll-rebuild']);
      gulp.watch('_jadefiles/**/*.jade', ['jade']);
      gulp.watch('_pugfiles/**/*.pug', ['pug']);
+     gulp.watch(['_site/**/*.html'], ['compress-html']);
  });
-
-
 
 /**
  * Default task, running just `gulp` will compile the sass,
  * compile the jekyll site, launch BrowserSync & watch files.
  */
 gulp.task('default', ['browser-sync', 'watch']);
+
+//
+// var project = {
+//   name:'mom',
+//   url:'http://jeansoto.com',
+// };
+// var paths = {
+//   source  : './',
+//   assets   : './assets',
+//   files   : './files',
+//   vendor  : './vendor',
+//   site    : './_site',
+//   bower   : './bower-components'
+// };
+//
+// var replaceFileName = {
+//   css: [ project.name + '.css', project.name + '.min.css' ],
+//   js:  [ project.name + '.js', project.name + '.min.js' ]
+// };
+//
+// var del           = require('del');
+// var runSequence   = require('run-sequence');
+//
+//
+// // JavaScript
+// gulp.task('js', function() {
+//   return gulp.src('assets/js/**')
+//     .pipe(plumber())
+//     .pipe(sourcemaps.init())
+//     .pipe(concat('assets/js/**'))
+//     .pipe($.sourcemaps.write('assets/js/**'))
+//     .pipe(gulp.dest('assets/js/**'));
+// });
+//
+// // Lint JavaScript
+// gulp.task('jshint', function() {
+//   return gulp.src('assets/js/**')
+//     .pipe(reload({
+//       stream: true,
+//       once: true
+//     }))
+//     .pipe(jshint())
+//     .pipe(jshint.reporter('jshint-stylish'))
+//     .pipe($.if(!browserSync.active, $.jshint.reporter('fail')));
+// });
+
+
+
+
+
+// gulp.task('build-js', function() {
+//     return gulp.src('_site/js/*.js')
+//         .pipe(jsValidate())
+//         .pipe(uglify())
+//         .pipe(gulp.dest('_site/js/'));
+// });
+
+
+// check tasks
+gulp.task('check-html', function() {
+    return gulp.src('_site/*.html')
+        .pipe(html5Lint())
+        .pipe(htmlhint())
+        .pipe(bootlint());
+});
+
+//
+// var scsslint = require('gulp-scss-lint');
+// gulp.task('check-scss', function() {
+//     gulp.src([ 'assets/css/**/*.scss' ])
+//         .pipe(scsslint())
+// });
+
+gulp.task('check', [ 'check-html' ], function() {});
+
+
+
+
+/**
+ * PageSpeed Insights with reporting for both mobile and desktop
+ */
+var psi                = require('psi');
+var site               = 'http://www.html5rocks.com';
+var key                = '';
+
+gulp.task('mobile', function () {
+    return psi(site, {
+        // key: key
+        nokey: 'true',
+        strategy: 'mobile',
+    }).then(function (data) {
+        console.log('Speed score: ' + data.ruleGroups.SPEED.score);
+        console.log('Usability score: ' + data.ruleGroups.USABILITY.score);
+    });
+});
+
+gulp.task('desktop', function () {
+    return psi(site, {
+        nokey: 'true',
+        // key: key,
+        strategy: 'desktop',
+    }).then(function (data) {
+        console.log('Speed score: ' + data.ruleGroups.SPEED.score);
+    });
+});
+
+gulp.task('speed', ['desktop', 'mobile']);
