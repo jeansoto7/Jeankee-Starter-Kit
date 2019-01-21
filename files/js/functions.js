4e4f88024ce434c38fc893e7e0ad010f3f960e29
jeansoto7 <jeancarlossoto7@gmail.com>
2019-01-20 20:46:59 -0400

    First Commit

--- files/js/functions.js
+++ files/js/functions.js
@@ -1,18 +1,53 @@
+<<<<<<< HEAD
+// Signup Forms
+$(function(){
+  $('.form-display .mc-field-group input').focusout(function(){
+    var text_val = $(this).val();
+    if(text_val === "") {
+      $(this).removeClass('has-value');
+    } else {
+      $(this).addClass('has-value');
+    }
+  });
+=======
 $(function() {
-  // 
+  // dsss
   smoothScroll(300);
+>>>>>>> 11867e35e751920c1c4a62bdcfa6618dedbb95fe
 });
 
-function smoothScroll (duration) {
-	$('a[href^="#"]').on('click', function(event) {
+// Stop Image being Draggable
+$('img').attr('draggable', 'false');
 
-	    var target = $( $(this).attr('href') );
+// Glitch
+function toggleGlitch(){
 
-	    if( target.length ) {
-	        event.preventDefault();
-	        $('html, body').animate({
-	            scrollTop: target.offset().top
-	        }, duration);
-	    }
+	var $glitchContainter = $('.sliced-content');
+  var $glitchDiv =  $('.glitch');
+	var $glitchIn =  $('.glitchIn');
+
+	$glitchDiv.mgGlitch({
+	  destroy: false,
+	  glitch: true,
+	  scale: true,
+	  blend: true,
+	  blendModeType: "hue",
+	  glitch1TimeMin: 200,
+	  glitch1TimeMax: 400,
+	  glitch2TimeMin: 10,
+	  glitch2TimeMax: 100
 	});
+	setTimeout(function(){
+		$glitchDiv.mgGlitch({
+			destroy: true
+		});
+	}, 1000);
+
+	setInterval(function(){
+      $glitchIn.toggleClass('glitchOut');
+  }, 1000);
 }
+
+$(function(){
+  setInterval(function(){toggleGlitch();}, 4000);
+});
